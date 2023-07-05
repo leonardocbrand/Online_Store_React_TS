ebac9ab31edcfc6bc6000762fae040428a5af19f
Alan Jorge <alan-jorge.1991@hotmail.com>
2023-07-05 17:05:36 -0300

    feat: group-11- requisito 3

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
+import ShoppingCart from '../pages/Shopping-cart';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={ <HomePage /> } />
+      <Route path="/shopping-cart" element={ <ShoppingCart /> } />
     </Routes>
   );
 }
