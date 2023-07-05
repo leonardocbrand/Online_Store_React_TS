d93cc450e4537cc4470317b3c5ffcfb20bf0492f
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-07-05 18:55:24 -0300

    feat: new component

---
+++ src/components/SearchList.tsx
@@ -0,0 +1,30 @@
+import { ProductsData } from '../types';
+
+type SearchListProps = {
+  products: ProductsData[];
+};
+
+function SearchList({ products }: SearchListProps) {
+  return (
+    <section>
+      {
+        products.length === 0
+          ? (
+            <h2 data-testid="home-initial-message">
+              Digite algum termo de pesquisa ou escolha uma categoria.
+            </h2>
+          ) : (
+            products.map((product: ProductsData) => (
+              <div key={ product.id } data-testid="product">
+                <p>{product.title}</p>
+                <img src={ product.thumbnail } alt="" />
+                <p>{`${product.currency_id} ${product.price}`}</p>
+              </div>
+            ))
+          )
+        }
+    </section>
+  );
+}
+
+export default SearchList;
