f371acf919b0f0807aa00592924bfb511bae7c2a
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-07-05 16:06:06 -0300

    feat: Requisito 02

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+function HomePage() {
+  return (
+    <main>
+      <input type="text" name="" id="" />
+      <h2 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h2>
+    </main>
+  );
+}
+
+export default HomePage;
