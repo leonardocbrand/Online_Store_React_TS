156bf75c2fe02a6f40cd38c1527ea0f6a814710c
¨Filipe <7filipe093@gmail.com>
2023-07-06 16:39:11 -0300

    req 7 ok

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { ProductsData } from '../types';
 
 type SearchListProps = {
@@ -5,24 +7,29 @@ type SearchListProps = {
 };
 
 function SearchList({ products }: SearchListProps) {
+  const [productDetails, setProductDetails] = useState({});
   return (
     <section>
-      {
-        products.length === 0
-          ? (
-            <h2 data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-            </h2>
-          ) : (
-            products.map((product: ProductsData) => (
-              <div key={ product.id } data-testid="product">
+      {products.length === 0 ? (
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+      ) : (
+        products.map((product: ProductsData) => (
+          <div key={ product.id } data-testid="product">
+            <Link
+              data-testid="product-detail-link"
+              to={ `/details/${product.id}` }
+            >
+              <div>
                 <p>{product.title}</p>
                 <img src={ product.thumbnail } alt="" />
                 <p>{`${product.currency_id} ${product.price}`}</p>
               </div>
-            ))
-          )
-        }
+            </Link>
+          </div>
+        ))
+      )}
     </section>
   );
 }
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import ShoppingCart from '../pages/Shopping-cart';
+import DetailsPage from '../components/DetailsPage';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={ <HomePage /> } />
       <Route path="/shopping-cart" element={ <ShoppingCart /> } />
+      <Route path="/details/:idDetails" element={ <DetailsPage /> } />
     </Routes>
   );
 }
