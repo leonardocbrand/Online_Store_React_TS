1d585c4680bc5a5b868abea9b9066c5eec59baed
Italo Araujo <italojsouza@gmail.com>
2023-07-07 17:12:42 -0300

    feat: requisito 12

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,120 +1,139 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-type ReviewsType = {
-  email: string;
-  message?: string;
-  rating: string;
+const INITIAL_STATE = {
+  email: '',
+  message: '',
+  rating: '',
 };
 
+const productId = 'reviews';
+
 function Rating() {
-  const [email, setEmail] = useState('');
-  const [rate, setRate] = useState('');
-  const [message, setMessage] = useState('');
-  const [reviews, setReviews] = useState<ReviewsType[]>([]);
+  const [formData, setFormData] = useState(INITIAL_STATE);
+  const [isInvalid, setIsInvalid] = useState(false);
 
-  useEffect(() => {
-    const oldReviews = JSON.parse(localStorage.getItem('reviews') ?? 'null');
-    if (oldReviews) {
-      setReviews(oldReviews);
-    }
-  }, []);
-
-  const handleChangeEmail = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(target.value);
-  };
-
-  const handleClickRadio = ({ target }: React.MouseEvent<HTMLInputElement>) => {
-    setRate((target as HTMLInputElement).value);
-  };
-
-  const handleChangeMessage = ({ target }: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(target.value);
+  const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    const Newreview = {
-      email,
-      message,
-      rating: rate,
-    };
 
-    setReviews([...reviews, Newreview]);
+    if (formData.email.length === 0
+      || !(formData.email.includes('@'))
+      || formData.rating.length === 0) {
+      setIsInvalid(true);
+      return;
+    }
 
-    localStorage.setItem('reviews', JSON.stringify(reviews));
-    setEmail('');
-    setRate('');
-    setMessage('');
-    const radio = document.getElementsByName('rate') as NodeListOf<HTMLInputElement>;
-    for (let i = 0; i < radio.length; i++) { radio[i].checked = false; }
+    if (localStorage.getItem(productId)) {
+      const arrayReviews = JSON.parse(localStorage.getItem(productId) as string);
+      localStorage.setItem(productId, JSON.stringify([...arrayReviews, formData]));
+    } else {
+      localStorage.setItem(productId, JSON.stringify([formData]));
+    }
+    setIsInvalid(false);
+    setFormData(INITIAL_STATE);
   };
+
+  const reviews = JSON.parse(localStorage.getItem(productId) as string);
 
   return (
     <div>
       <h3>Avaliações</h3>
+      {(isInvalid)
+      && (
+        <div>
+          <p data-testid="error-msg">Campos inválidos</p>
+        </div>
+      )}
       <form>
         <input
+          data-testid="product-detail-email"
           type="email"
           name="email"
           id="email"
           placeholder="Email"
           required
-          value={ email }
-          onChange={ handleChangeEmail }
+          value={ formData.email }
+          onChange={ handleChange }
         />
-        <div className="rate">
+        <div className="rating">
           <input
+            data-testid="1-rating"
             type="radio"
             id="star1"
-            name="rate"
+            name="rating"
             value="1"
             required
-            onClick={ handleClickRadio }
+            onChange={ handleChange }
           />
           <label htmlFor="star1" title="text">1 ★</label>
           <input
+            data-testid="2-rating"
             type="radio"
             id="star2"
-            name="rate"
+            name="rating"
             value="2"
-            onClick={ handleClickRadio }
+            onChange={ handleChange }
           />
           <label htmlFor="star2" title="text">2 ★</label>
           <input
+            data-testid="3-rating"
             type="radio"
             id="star3"
-            name="rate"
+            name="rating"
             value="3"
-            onClick={ handleClickRadio }
+            onChange={ handleChange }
           />
           <label htmlFor="star3" title="text">3 ★</label>
           <input
+            data-testid="4-rating"
             type="radio"
             id="star4"
-            name="rate"
+            name="rating"
             value="4"
-            onClick={ handleClickRadio }
+            onChange={ handleChange }
           />
           <label htmlFor="star4" title="text">4 ★</label>
           <input
+            data-testid="5-rating"
             type="radio"
             id="star5"
-            name="rate"
+            name="rating"
             value="5"
-            onClick={ handleClickRadio }
+            onChange={ handleChange }
           />
           <label htmlFor="star5" title="text">5 ★</label>
         </div>
-        <textarea
+        <input
+          data-testid="product-detail-evaluation"
           name="message"
-          cols={ 30 }
-          rows={ 5 }
           placeholder="Mensagem (Opcional)"
-          onChange={ handleChangeMessage }
-          value={ message }
+          onChange={ handleChange }
+          value={ formData.message }
         />
-        <button onClick={ handleSubmit }>Avaliar</button>
+        <button data-testid="submit-review-btn" onClick={ handleSubmit }>Avaliar</button>
       </form>
+      {(reviews)
+      && (
+        <div>
+          {reviews.map((item : { email:string; rating:string; message:string; }) => {
+            return (
+              <div key={ item.email }>
+                <p data-testid="review-card-email">{item.email}</p>
+                {(item.rating === '1') && (<p data-testid="review-card-rating">★</p>)}
+                {(item.rating === '2') && (<p data-testid="review-card-rating">★★</p>)}
+                {(item.rating === '3') && (<p data-testid="review-card-rating">★★★</p>)}
+                {(item.rating === '4') && (<p data-testid="review-card-rating">★★★★</p>)}
+                {(item.rating === '5') && (<p data-testid="review-card-rating">★★★★★</p>)}
+                <p data-testid="review-card-evaluation">{item.message}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
