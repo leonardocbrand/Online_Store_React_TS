b2918b68fab7b51e0c8ff6027af6d8e75bdcf19d
Leonardo C. Brandao <leonardocbrand@gmail.com>
2023-07-20 23:49:08 -0300

    Delete src/__tests__ directory

--- src/__tests__/helpers/renderWithRouter.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
-import { render } from '@testing-library/react';
-
-const renderWithRouter = (component: React.ReactElement, initialPath = '/') => {
-  return render(<MemoryRouter initialEntries={ [initialPath] }>{component}</MemoryRouter>);
-};
-export default renderWithRouter;
